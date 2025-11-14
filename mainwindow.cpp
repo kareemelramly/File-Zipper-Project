5693eb96ea33af049d3f667035f7c3c961831163
Shahd-feky <shahdelfeky@aucegypt.edu>
2025-11-15 00:03:05 +0200

    QT

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include "QMessageBox"
+#include "QFileDialog"
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -12,3 +13,11 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_Compress_clicked()
+{
+    QString filename= QFileDialog::getOpenFileName(this, "Select and open file", "C://");
+    QMessageBox::information(this, "..", filename);
+
+}
+
